102d2e59d8f0fb0576549afcd2eba901245d0e77
moonhan <hotdog19k2@naver.com>
2016-12-13 14:53:38 +0900

    master에서 추가

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -3,5 +3,7 @@ package kr.or.dgit.bigdata.helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello java");
+		System.out.println("master에서 추가");
 	}
 }
