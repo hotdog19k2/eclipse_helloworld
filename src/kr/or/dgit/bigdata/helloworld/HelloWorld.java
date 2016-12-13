6442cbab95ceed7d8046875fcfc7a97c503267ed
moonhan <hotdog19k2@naver.com>
2016-12-13 15:13:49 +0900

    한줄 추가

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("의도적으로 충돌작업");
+		
+		System.out.println("GitHub와의 연결작업1");
+		
 	}
 }
